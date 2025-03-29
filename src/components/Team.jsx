d11d4102cf5d02bec08878ebfa5d22980359e501
MendezOscar <7mendez.oscar@gmail.com>
2025-03-29 17:39:22 -0600

    agregando git ignore

---
+++ src/components/Team.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Address</h2>
+          <p>Visit us, it will be a pleasure to attend you.</p>
+        </div>
+        <div id="row">
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=12002%20Centerhill%20St,%20Silver%20Spring,%20MD%2020902+(Ebenezer%20Remodeling%20and%20Painting%20LLC)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/collections/drones/">drones ireland</a></iframe>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
---
+++ src/components/services.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Our Services</h2>
+          <p>
+          Our fully licensed crew uses high-quality materials and meticulous craftsmanship to deliver stunning results for both residential and commercial spaces. Let us help you create a beautiful, inviting space inside and out that reflects your style and enhances your property's value.
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
